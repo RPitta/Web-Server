0cabe0aa2be91433bb07c8176fcba6414e22160f
Rodrigo <rodrigo.pittar@gmail.com>
2019-04-24 12:24:20 -0400

    Added file

--- server.c
+++ server.c
@@ -1,19 +1,3 @@
-/* server.c
-A basic web server written in C that implements the HTTP protocol using the native network sockets library.
-The web server also utilizes multithreading to process multiple requests concurrently.
-
-The web server:
-- Accepts TCP connections from web browsers
-- Reads in the packets that the browsers send
-- Detects the ends of requests by checking the contents of the packets
-- Parses the request to determine the type of HTTP connection (persistent or non-persistent)
-- Prepares and then sends a response to the web browser which then displays the content being requested. If the request is invalid, the response sends
-  an appropriate error message
-
-Usage:
-	gcc -o server server.c
-	./server <an available port number>
-*/
 #include <sys/socket.h>
 #include <sys/sendfile.h>
 #include <netinet/in.h>
